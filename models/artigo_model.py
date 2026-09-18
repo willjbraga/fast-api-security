ef65170741a2953e993de06a25de33cb98d5f59a
William Braga de Oliveira Junior <williamjubraga@gmail.com>
2026-09-18 13:26:18 -0300

    concertando e atualizando código para evidar futuros problemas

--- models/artigo_model.py
+++ models/artigo_model.py
@@ -1,14 +1,16 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
+
+from sqlalchemy.orm import Mapped, mapped_column
 
 from core.configs import Base
 
 class ArtigoModel(Base):
     __tablename__ = 'artigos'
     
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    titulo = Column(String(256))
-    descricao = Column(String(256))
-    url_fonte = Column(String(256))
-    usuario_id = Column(Integer, ForeignKey('usuario.id'))
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    titulo: Mapped[str] = mapped_column(String(256), nullable=False)
+    descricao: Mapped[str] = mapped_column(String(256), nullable=False)
+    url_fonte: Mapped[str] = mapped_column(String(256), nullable=False)
+    usuario_id: Mapped[int] = mapped_column(Integer, ForeignKey('usuarios.id'), nullable=False)
     criador = relationship("UsuarioModel", back_populates='artigos', lazy='joined')
--- models/usuario_model.py
+++ models/usuario_model.py
@@ -1,17 +1,25 @@
 from sqlalchemy import Integer, String, Column, Boolean
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, mapped_column, Mapped
+from sqlalchemy.dialects.postgresql import ENUM as PGEnum
+
+from enum import Enum
 
 from core.configs import Base
+
+class TipoAcessoEnum(str, Enum):
+    ADMIN = "admin"
+    MANAGER = "manager"
+    USER = "user"
 
 class UsuarioModel(Base):
     __tablename__ = 'usuarios'
     
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    nome = Column(String(256), nullable=True)
-    sobrenome = Column(String(256), nullable=True)
-    email = Column(String(256), index=True, nullable=False, unique=True)
-    senha = Column(String(256), nullable=False)
-    eh_admin = Column(Boolean, default=False)
+    id: Mapped[int] = mapped_column(primary_key=True)
+    nome: Mapped[str] = mapped_column(String(256), nullable=False)
+    sobrenome: Mapped[str | None] = mapped_column(String(256), nullable=True)
+    email: Mapped[str] = mapped_column(String(256), index=True, nullable=False, unique=True)
+    senha: Mapped[str] = mapped_column(String(256), nullable=False)
+    acesso: Mapped[TipoAcessoEnum] = mapped_column(PGEnum(TipoAcessoEnum, name= "tipo_acesso_enum", values_callable=lambda enum: [item.value for item in enum],), default=TipoAcessoEnum.USER, nullable= False)
     artigos = relationship(
         "ArtigoModel",
         cascade="all,delete-orphan",
